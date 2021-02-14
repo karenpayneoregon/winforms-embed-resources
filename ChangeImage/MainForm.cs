7f7eb4b9cc8d82ed85758ec63d8002943e9d2730
Karen Payne <Karen.1.Payne@oregon.gov>
2021-02-14 11:16:33 -0800

    Add project files.

---
+++ ChangeImage/MainForm.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ChangeImage.Classes;
+using ChangeImage.Extensions;
+
+namespace ChangeImage
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            
+            Shown += OnShown;
+        }
+
+        private void OnShown(object? sender, EventArgs e)
+        {
+            var allImages = ResourceImages.Instance.Images();
+            
+            /*
+             * Do we have images from resources?
+             */
+            if (allImages is null)
+            {
+                MessageBox.Show("No images located");
+                SelectFromAllImagesButton.Enabled = false;
+            }
+            
+            AllImagesListBox.DataSource = allImages;
+
+            ChangeFromAllImage();
+
+            IconListBox.DataSource = allImages.Icons();
+
+            BitmapListBox.DataSource = allImages.BitMaps();
+        }
+
+        private void SelectFromAllImagesButton_Click(object sender, EventArgs e)
+        {
+            ChangeFromAllImage();
+        }
+
+        private void ChangeFromAllImage()
+        {
+            if (AllImagesListBox.SelectedIndex <= -1) return;
+
+            var item = (ResourceItem) AllImagesListBox.SelectedItem;
+            pictureBox1.SizeMode = item.IsIcon ? PictureBoxSizeMode.Normal : PictureBoxSizeMode.Zoom;
+            pictureBox1.Image = item.Image;
+        }
+    }
+}
